0c5930eb8c8964b9512d836691ac761ff99c1864
Felix M <28467515+FelixM-Infosec@users.noreply.github.com>
2019-05-26 17:15:31 +0000

    Add files via upload

--- quakesimple.php
+++ quakesimple.php
@@ -5,7 +5,7 @@
 		h1           {font-size: 60pt; font-family: Calibri; color: white }
 		body         {background-image: url('assets/pictures/blackDots.png');}
 		.column      {float: left; width: 33.33%; font-size: 40pt; font-family: Calibri; color: white;}
-		.row:after   {float: center; content: ""; display: table; clear: both;}
+		.row:after   {float: center; content: ""; display: table; clear: both; font-size: 12pt; font-family: Calibri, Helvetica, sans-serif; color: white;}
 		.footer      {position:absolute; bottom:2; align: right; font-size: 12pt; font-family: Calibri, Helvetica, sans-serif; color: white;}
 		.link        {font-size: 12pt; font-family: Calibri, Helvetica, sans-serif; color: white;}
 	</style>
@@ -41,10 +41,24 @@
 		
 		echo"<div>
 				<a class='link' href='/quake.php'>Standard Version</a>
-				<center><input type=button value='RE-ROLL' onClick='window.location.reload()'><input type='text' value='$paste' id='copybox' size='70' align='center'><button onclick='copyclip()'>COPY</button></center><br>
-				<div><br></div><br><h1><center>", $map, "</center></h1><br>
+				<center><input type=button value='RE-ROLL' onClick='window.location.reload()'><input type='text' value='$paste' id='copybox' size='70' align='center'><button onclick='copyclip()'>COPY</button></center>
+				<div></div><h1><center>", $map, "</center></h1>
 			</div>";
-		
+		echo"<div class='row'> 
+				<div class='column' align='right'>
+					$p1c1<br>
+					$p1c2<br>
+					$p1c3<br>
+				</div>
+					<div class='column'>
+						<center><br>VS</center>
+					</div>
+				<div class='column' align='left'>
+					$p2c1<br>
+					$p2c2<br>
+					$p2c3<br>
+				</div>
+			</div>";
 	?>
 <footer class='footer'></footer>
 </body>
